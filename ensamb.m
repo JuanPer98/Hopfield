1886810bf479369873cb7de7c057e1f93d6f2c20
JuanPer98 <jljpo7@hotmail.com>
2025-07-27 18:12:11 -0600

    [UPDATE][DOC][FEAT] Initial updates
    UPDATE:
    	- Patterns moved to dedicated pattern folder.
    DOC:
    	- Improved comments on code logic.
    FEAT:
    	- Improved file management logic.

--- ensamb.m
+++ ensamb.m
@@ -1,6 +1,7 @@
 function [ M ] = ensamb( m,d1,d2 )
-%ensamblar matrices
-%   Detailed explanation goes here
+%Assemble columnized matrix on the desired dimensions
+%   Takes the columnized matrix m (vector) and turns it into a matrix of
+%   d1 x d2
 
 M=zeros(d1,d2);
 d=0;
